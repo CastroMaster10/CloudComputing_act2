e5f718d6080bfbea37a50f90d24ac68cfef1f0b7
Pedro Fernández Merino <167809308+PedroFerMer03@users.noreply.github.com>
2025-04-04 18:31:42 -0600

    api

--- src/score.py
+++ src/score.py
@@ -7,18 +7,18 @@ from azureml.core.model import Model
 
 def init():
   global model
-  model_path = Model.get_model_path('modelo')
+  model_path = Model.get_model_path('model')
   model = joblib.load(model_path)
 
 def run(raw_data):
-  try: ## Try la predicción.
+  try: ## Try la predicciï¿½n.
     data = json.loads(raw_data)['data'][0]
     data = pd.DataFrame(data)
 
 
-    result = model.predict(data_dummies).tolist()
-    result_sigmoid = sigmoid(result)
-    umbral = 0.5444075450750484
+    result = model.predict(data).tolist()
+    result_sigmoid = np.sigmoid(result)
+    umbral = 0.5260736508016486
     result_finals = [1 if x > umbral else 0 for x in result_sigmoid]
 
     return json.dumps(result_finals)
